a9c17b7e3a7066ce732fec379f8f97f00bab20cb
archistico <info@archistico.com>
2017-10-24 18:49:28 +0200

    home ricerca ok

--- src/AppBundle/Controller/EbookController.php
+++ src/AppBundle/Controller/EbookController.php
@@ -22,21 +22,33 @@ class EbookController extends Controller
         
         $form = $this->createFormBuilder($ebook)
             ->add('codice', TextType::class, array('label' => 'Inserisci il codice del cartoncino'))
-            ->add('save', SubmitType::class, array('label' => 'Crea ebook', 'attr' => array('class' => 'btn-primary btn-block')))
+            ->add('find', SubmitType::class, array('label' => 'Cerca ebook', 'attr' => array('class' => 'btn-primary btn-block')))
             ->getForm();
         
         $form->handleRequest($request);
             
         if ($form->isSubmitted() && $form->isValid()) {
             
-            $ebook = $form->getData();
-            $ebook->setEpub('file.epub');
+            $codice = $form->getData()->getCodice();
 
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($ebook);
-            $em->flush();
+            $ebook = $this->getDoctrine()
+                ->getRepository(Ebook::class)
+                ->findOneBy(
+                    array('codice' => $codice)
+                );
+    
+            if (!$ebook) {
+                $this->addFlash(
+                    'notice',
+                    'Nessun ebook con questo codice!'
+                );
 
-            return $this->redirect($this->generateUrl('homepage'));
+                return $this->redirectToRoute('homepage');                
+            }
+    
+            return $this->render('AppBundle:Ebook:download.html.twig', array(
+                'ebook' => $ebook,
+            ));
         }
 
         return $this->render('AppBundle:Ebook:index.html.twig', array(
@@ -45,6 +57,39 @@ class EbookController extends Controller
         
         
     }
+
+    /**
+     * @Route("/nuovo", name="nuovo")
+     */
+    public function nuovoAction(Request $request)
+    {
+        $ebook = new Ebook();
+        
+        $form = $this->createFormBuilder($ebook)
+            ->add('codice', TextType::class, array('label' => 'Inserisci il codice del cartoncino'))
+            ->add('epub', TextType::class, array('label' => 'File epub'))
+            ->add('save', SubmitType::class, array('label' => 'Crea ebook', 'attr' => array('class' => 'btn-primary btn-block')))
+            ->getForm();
+        
+        $form->handleRequest($request);
+            
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $ebook = $form->getData();
+            
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($ebook);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('homepage'));
+        }
+
+        return $this->render('AppBundle:Ebook:nuovo.html.twig', array(
+            'form' => $form->createView(),
+        ));
+                
+    }
+
 
     /**
      * @Route("/download/{codice}", name="download")
