8ad39c964bda9c4e3c0f0864fd2f3ef8b4bf1858
archistico <info@archistico.com>
2017-10-30 21:16:44 +0100

    entita codici

---
+++ src/AppBundle/Entity/Codici.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Codici
+ *
+ * @ORM\Table(name="codici")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CodiciRepository")
+ */
+class Codici
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="codice", type="string", length=255, unique=true)
+     */
+    private $codice;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="download", type="integer")
+     */
+    private $download;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set codice
+     *
+     * @param string $codice
+     *
+     * @return Codici
+     */
+    public function setCodice($codice)
+    {
+        $this->codice = $codice;
+
+        return $this;
+    }
+
+    /**
+     * Get codice
+     *
+     * @return string
+     */
+    public function getCodice()
+    {
+        return $this->codice;
+    }
+
+    /**
+     * Set download
+     *
+     * @param integer $download
+     *
+     * @return Codici
+     */
+    public function setDownload($download)
+    {
+        $this->download = $download;
+
+        return $this;
+    }
+
+    /**
+     * Get download
+     *
+     * @return int
+     */
+    public function getDownload()
+    {
+        return $this->download;
+    }
+}
+
--- src/AppBundle/Entity/Costanti.php
+++ src/AppBundle/Entity/Costanti.php
@@ -5,5 +5,5 @@ namespace AppBundle\Entity;
 class Costanti {
 
     const SALT = 'Berlino1510';
-
+    const MAXDOWNLOAD = 3;
 }
